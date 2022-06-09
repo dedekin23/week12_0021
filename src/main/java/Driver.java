05628beee5f66829e5f767850cb6884e12392d73
dedekin23 <michael.manullang29@gmail.com>
2022-06-09 20:52:41 +0700

    update driver java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -8,7 +8,7 @@ public class Driver {
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.increment();
+		counter.increment()
 		
 		System.out.println("Current count:" + counter.getCount());
 		
